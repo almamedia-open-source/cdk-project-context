50455ce40e647026a3b27bb9152a58be98e4d2ce
Ari Palo <ari.palo@almamedia.fi>
2022-01-08 19:14:15 +0200

    chore: organize things

--- .projenrc.js
+++ .projenrc.js
@@ -3,19 +3,28 @@ const { AwsCdkConstructLibrary, TextFile } = require('projen');
 const nodejsVersion = '14.17.6';
 
 const project = new AwsCdkConstructLibrary({
+
+  // Metadata
   stability: 'experimental',
   authorName: 'Alma Media',
   authorOrganization: true,
   authorAddress: 'opensource@almamedia.dev',
-  cdkVersion: '2.0.0',
-  defaultReleaseBranch: 'main',
   name: '@almamedia-open-source/cdk-project-context',
   repositoryUrl: 'https://github.com/almamedia-open-source/cdk-project-context.git',
-  minNodeVersion: nodejsVersion,
-  constructsVersion: '10.0.0',
+  keywords: ['cdk', 'aws-cdk', 'awscdk', 'aws'],
 
+  // CI/CD
+  defaultReleaseBranch: 'main',
+
+  // Dependencies
+  minNodeVersion: nodejsVersion,
+  cdkVersion: '2.0.0',
+  constructsVersion: '10.0.0',
   peerDeps: ['constructs', 'aws-cdk-lib'],
   devDeps: ['constructs', 'aws-cdk-lib'],
+
+  // Gitignore
+  gitignore: ['.DS_Store'],
 
 });
 

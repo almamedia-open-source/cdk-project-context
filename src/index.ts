ff89ac061a1b1f0a0c468d78e5fbd9ccf3c9c8da
Ari Palo <ari.palo@almamedia.fi>
2022-02-08 23:47:28 +0200

    feat: export envregexp

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { Project, ProjectProps } from './project';
 export { ProjectContext, ProjectContext as PC } from './project-context';
 export { AccountType } from './account-type';
 export { EnvironmentType } from './environment-type';
+export { EnvRegExp } from './envregexp';
